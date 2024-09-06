67e7272590673be68a478825ab6c97930ad82c1b
unixkiwi <kiwi455@outlook.de>
2024-09-06 16:08:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo apt install git wget curl nano bspwm sxhkd hsetroot blueman \
 	pulseaudio polybar picom nala dunst lxpolkit xorg doublecmd-gtk \
 	rofi nwg-look neofetch gpg alacritty lightdm fonts-font-awesome \
 	python3 pavucontrol pipx bluez eza less network-manager libnotify-bin psmisc \
-	nm-tray copyq
+	nm-tray copyq flameshot
 pipx install pywal
